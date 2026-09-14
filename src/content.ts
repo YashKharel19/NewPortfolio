bfc6590f051f51741a3f153fe3478332decc855b
Yash Kharel <yash048339@gmail.com>
2026-09-14 14:15:57 -0600

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -3,7 +3,7 @@
 // Easily edit text, links, photos, products, and jobs in this single file.
 // ============================================================================
 
-import yashPhoto from './assets/images/YK.MOV';
+import yashPhoto from './assets/images/YK.JPG';
 import lumashaPhoto from './assets/images/lumasha.png';
 import bagishaPhoto from './assets/images/bagisha.png';
 
